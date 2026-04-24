fac78585858be63e2a03491d7c6cdb134b8bc8b2
Tyler Braham <tjbraham@uw.edu>
2026-04-24 15:09:39 -0700

    modified:   BF16_Arithmetic/bf16_addition.sv

--- BF16_Arithmetic/bf16_addition.sv
+++ BF16_Arithmetic/bf16_addition.sv
@@ -1,13 +1,20 @@
-module bf16_adder (a, b, result);
+module bf16_add (a, b, result);
     input [15:0] a, b;
     output [15:0] result;
 
     logic sign_a, sign_b;
-    logic [7:0] mantissa_a, mantissa_b;
+    logic [6:0] mantissa_a, mantissa_b;
     logic [7:0] exp_a, exp_b;
 
     assign sign_a = a[15];
     assign sign_b = b[15];
 
+    assign exp_a = a[14:7];
+    assign exp_b = b[14:7];
+
+    assign mantissa_a = {1'b1, a[6:0]};
+    assign mantissa_b = {1'b1, b[6:0]};
+
+    
 
 endmodule
