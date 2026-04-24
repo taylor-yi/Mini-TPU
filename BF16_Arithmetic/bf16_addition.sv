d206278179b5619660284cb9df3a5fee846fe1e1
Tyler Braham <tjbraham@uw.edu>
2026-04-24 15:22:58 -0700

    compare

--- BF16_Arithmetic/bf16_addition.sv
+++ BF16_Arithmetic/bf16_addition.sv
@@ -3,7 +3,7 @@ module bf16_add (a, b, result);
     output [15:0] result;
 
     logic sign_a, sign_b;
-    logic [6:0] mantissa_a, mantissa_b;
+    logic [7:0] mantissa_a, mantissa_b;
     logic [7:0] exp_a, exp_b;
 
     assign sign_a = a[15];
@@ -15,6 +15,33 @@ module bf16_add (a, b, result);
     assign mantissa_a = {1'b1, a[6:0]};
     assign mantissa_b = {1'b1, b[6:0]};
 
-    
+    // --------------------------------------------------------
+    // 2. FIND THE LARGER NUMBER (To know who shifts)
+    // --------------------------------------------------------
+    logic a_is_larger;
+    logic [7:0] exp_large, exp_small;
+    logic [7:0] mant_large_start, mant_small_start;
+    logic sign_large, sign_small;
+
+    always_comb begin
+        // Compare exponents first. If equal, compare mantissas.
+        if ((exp_a > exp_b) || ((exp_a == exp_b) && (mant_a >= mant_b))) begin
+            a_is_larger      = 1'b1;
+            exp_large        = exp_a;
+            exp_small        = exp_b;
+            mant_large_start = mant_a;
+            mant_small_start = mant_b;
+            sign_large       = sign_a;
+            sign_small       = sign_b;
+        end else begin
+            a_is_larger      = 1'b0;
+            exp_large        = exp_b;
+            exp_small        = exp_a;
+            mant_large_start = mant_b;
+            mant_small_start = mant_a;
+            sign_large       = sign_b;
+            sign_small       = sign_a;
+        end
+    end
 
 endmodule
