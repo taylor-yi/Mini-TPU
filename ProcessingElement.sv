00b9767530748cdbde31e04f1c4f9a950cb8a1aa
Taylor Yi <taykeahisy@gmail.com>
2026-04-29 15:28:07 -0700

    new file:   ProcessingElement.sv

---
+++ ProcessingElement.sv
@@ -0,0 +1,8 @@
+module processing_element(a, b, result);
+    input logic [15:0] a, b;
+    output logic [15:0] result;
+
+    // First multiply a and b together
+
+    // Add to a global sum and then push the result once the completion is completed
+endmodule
