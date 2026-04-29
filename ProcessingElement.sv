d8295e597661e4ac8bc88c286120537fc1fb2fb5
Taylor Yi <taykeahisy@gmail.com>
2026-04-29 16:01:27 -0700

    modified:   ProcessingElement.sv

--- ProcessingElement.sv
+++ ProcessingElement.sv
@@ -8,11 +8,11 @@ module processing_element(a, b, result, clk, rst, en, clear);
     logic [15:0] add_out;
     logic [15:0] accumulator;
 
-    bf16_mul PE_Mul(.a(a), .b(b), .result(mul_output));
+    bf16_mul PE_Mul (.a(a), .b(b), .result(mul_output));
 
     logic [15:0] result_next;
 
-    bf16_add PE_Add(.a(mult_output), .b(accumulator), .result(add_out));
+    bf16_add PE_Add (.a(mult_output), .b(accumulator), .result(add_out));
 
     // Add to a global sum and then push the result once the completion is completed
         always_ff @(posedge clk or negedge rst_n) begin
